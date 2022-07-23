1530aeb27a830e997dfe4aad65847ba6cfc429cb
izaiasmachado <izaiasmachado.dev@gmail.com>
2022-07-23 12:47:47 -0300

    feat: setup login and logoff

--- Interfaces/ILog.cs
+++ Interfaces/ILog.cs
@@ -4,6 +4,7 @@ namespace izaiasmachado_d3_avaliacao.Interfaces
 {
     internal interface ILog
     {
-        public void Create(User user);
+        public void CreateLogin(User user);
+        public void CreateLogoff(User user);
     }
 }
