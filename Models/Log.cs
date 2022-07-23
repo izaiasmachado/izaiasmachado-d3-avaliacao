f5fd5d55586431c5d2147c3ddf351108734d8566
izaiasmachado <izaiasmachado.dev@gmail.com>
2022-07-23 12:11:18 -0300

    feat: write log in file

---
+++ Models/Log.cs
@@ -0,0 +1,32 @@
+﻿namespace izaiasmachado_d3_avaliacao.Models
+{
+    internal class Log : Base
+    {
+        private const string path = "database/logs.txt";
+
+        public Log()
+        {
+            CreateFolderAndFile(path);
+        }
+
+        private static string PrepareLine(User user, int hour, int year, int month, int day)
+        {
+            string dayString = day.ToString().PadLeft(2, '0');
+            string monthString = month.ToString().PadLeft(2, '0');
+
+            return $"Usuário {user.Name} ({user.IdUser}) acessou o sistema às {hour} h do dia {dayString}/{monthString}/{year}";
+        }
+
+        public void Create(User user) {
+            DateTime moment = DateTime.Now;
+
+            int hour = moment.Hour;
+            int year = moment.Year;
+            int month = moment.Month;
+            int day = moment.Day;
+
+            string[] line = { PrepareLine(user, hour, year, month, day) };
+            File.AppendAllLines(path, line);
+        }
+    }
+}
